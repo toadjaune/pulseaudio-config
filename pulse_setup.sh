60a160b3a773718a9d81c36e0e7d371be9af7e62
Stephen Pollard <stephen@stephentpollard.com>
2020-06-10 12:47:31 -0600

    Added low latency option to loopbacks and changed some comments

--- pulse_setup.sh
+++ pulse_setup.sh
@@ -5,8 +5,8 @@
 # You can find their names with the following commands :
 # pacmd list-sources
 # pacmd list-source-outputs
-# Use pavucontrol to make tests for your setup, and to make the runtime configuration
-# Route you audio source to virtual1
+# Use pavucontrol to make tests for your setup and to make the runtime configuration
+# Route your audio source to virtual1
 # Record your sound (videoconference) from virtual2.monitor
 
 MICROPHONE="alsa_input.pci-0000_00_1b.0.analog-stereo"
@@ -14,17 +14,18 @@ SPEAKERS="alsa_output.pci-0000_00_1b.0.analog-stereo"
 
 
 # Create the null sinks
-# virtual1 gets your audio source (mplayer ...) only
-# virtual2 gets virtual1 + micro
+# virtual1 gets your audio sources (mplayer ...) that you want to hear and share
+# virtual2 gets all the audio you want to share (virtual1 + micro)
 pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
 pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
 
 # Now create the loopback devices, all arguments are optional and can be configured with pavucontrol
-pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS
-pactl load-module module-loopback source=virtual1.monitor sink=virtual2
-pactl load-module module-loopback source=$MICROPHONE sink=virtual2
+pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=1
+pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=1
+pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=1
 
-# If you struggle to find the correct values of your physical devices, you can also simply let these undefined, and configure evrything manually via pavucontrol
+# If you struggle to find the correct values of your physical devices, you can
+# also simply leave these undefined, and configure everything manually via pavucontrol
 # pactl load-module module-loopback source=virtual1.monitor
 # pactl load-module module-loopback source=virtual1.monitor sink=virtual2
 # pactl load-module module-loopback sink=virtual2
